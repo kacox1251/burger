eebce21a866fe30ffcb459710956c021b25cdb26
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2020-02-09 13:09:10 -0700

    creating orm for burgers

--- config/orm.js
+++ config/orm.js
@@ -3,3 +3,45 @@
     * `insertOne()`
     * `updateOne()`
 */
+
+const connection = require("../config/connection.js");
+
+const orm = {
+  all: function(table, cb) {
+    const queryString = "SELECT * FROM " + table + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create: function(table, column, value, cb) {
+    const queryString = `INSERT INTO ${table} (${column}, devoured) VALUES (${value}, false)`;
+
+    console.log(queryString);
+
+    connection.query(queryString, value, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  update: function(table, columnVal, condition, cb) {
+    const queryString = `UPDATE ${table} SET (${columnVal}) WHERE (${condition} false)`;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
